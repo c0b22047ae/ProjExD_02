75736750503657235e8f46a33c94b0ec7b7c14b1
unknown <c0b22047ae@edu.teu.ac.jp>
2023-11-28 14:55:54 +0900

    練習5衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -43,6 +43,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        if kk_rct.colliderect(bom_rct):
+            print("game over")
+            return
             
         key_lst = pg.key.get_pressed()
         sum_mv = [0,0]

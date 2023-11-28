bb3c73012b434502ff41f7b5a7a8a0ef8f2abd32
unknown <c0b22047ae@edu.teu.ac.jp>
2023-11-28 14:39:29 +0900

    練習3　こうかとんSurfaceを移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,13 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta ={  #練習3
+    pg.K_UP:(0,-5),  #
+    pg.K_DOWN:(0,+5),  #
+    pg.K_LEFT:(-5,0),  #
+    pg.K_RIGHT:(+5,0)  #
+}
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +19,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()  #練習3 こうかとんのSurfaceRectを抽出する
+    kk_rct.center = 900,400
     bom_img = pg.Surface((20,20))  #練習1　透明のSurfaceを作る
     pg.draw.circle(bom_img,(255,0,0),(10,10),10)  #練習１　赤い半径10の円を描く
     bom_img.set_colorkey((0,0,0))
@@ -25,9 +34,19 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for k,tpl in delta.items():
+            if key_lst[k]:  #練習3　キーが押されたら
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
+        
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        #screen.blit(kk_img, [900, 400])
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])  #練習３　こうかとんを移動させる
+        screen.blit(kk_img,kk_rct)
         bom_rct.move_ip(vx,vy)
         screen.blit(bom_img,bom_rct)
         pg.display.update()
